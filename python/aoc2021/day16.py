3bbf039d967069e1547f0736630a02ec470cbcd0
Dan Whitford <dan.whitford@fanduel.com>
2021-12-16 15:05:16 +0000

    --wip-- [skip ci]

--- python/aoc2021/day16.py
+++ python/aoc2021/day16.py
@@ -1,3 +1,4 @@
+import os.path
 
 hex_map = {
     '0': '0000',
@@ -19,7 +20,14 @@ hex_map = {
 }
 
 type_map = {
-    4: 'literal'
+    0: 'sum',
+    1: 'product',
+    2: 'min',
+    3: 'max',
+    4: 'literal',
+    5: 'gt',
+    6: 'lt',
+    7: 'eq',
 }
 
 def adder(a, b):
@@ -28,9 +36,28 @@ def adder(a, b):
 
 def expand(hex_string):
     return ''.join(
-        [hex_map[h] for h in hex_string]
+        [hex_map.get(h, '') for h in hex_string]
     )
 
+def sum_versions(packet):
+    total = 0
+    total += packet['version']
+
+    if packet['type'] != 'literal':
+        for child in packet['children']:
+            total += sum_versions(child)
+    
+    return total
+
+def eval_packet(packet):
+    t = packet['type']
+    if t == 'literal':
+        return packet['val']
+    elif t == 'sum':
+        r = 0
+        for c in packet['children']:
+            r += eval_packet(c)
+        return r
 class Reader():
     def __init__(self, packet_string):
         self.curr = 0
@@ -38,11 +65,36 @@ class Reader():
 
     def read_packet(self):
         version = int(self.read_n(3), 2)
-        type = type_map.get(int(self.read_n(3), 2), 'operator')
-        return {
-            'version': version,
-            'type': type,
-        }
+        packet_id = int(self.read_n(3), 2)
+        packet_type = type_map.get(packet_id, 'operator')
+
+        if packet_id == 4:
+            val = self.read_val()
+            return {
+                'version': version,
+                'type_id': packet_id,
+                'type': packet_type,
+                'val': val,
+            }
+        else:
+            packets = []
+            length_type_id = self.read()            
+            if length_type_id == '0':
+                l = int(self.read_n(15), 2)
+                read_up_to = self.curr + l
+                while self.curr < read_up_to:
+                    packets.append(self.read_packet())
+            elif length_type_id == '1':
+                l = int(self.read_n(11), 2)
+                packets_to_read = l
+                while len(packets) < packets_to_read:
+                    packets.append(self.read_packet())
+            return {
+                'version': version,
+                'type_id': packet_id,
+                'type': packet_type,
+                'children': packets,
+            }
 
     def read(self):
         c = self.src[self.curr]
@@ -54,3 +106,21 @@ class Reader():
         for i in range(n):
             s += self.read()
         return s
+
+    def read_val(self):
+        s = ''
+        while True:
+            bits = self.read_n(5)
+            s += bits[1:]
+            if bits[0] != '1':
+                break
+        return int(s, 2)
+
+
+if __name__ == '__main__':
+    with open(os.path.dirname(__file__) + '/res/day16') as f:
+        s = f.read()
+        h = expand(s)
+        reader = Reader(h)
+        packet = reader.read_packet()
+        print(sum_versions(packet))
--- python/aoc2021/test/test_day16.py
+++ python/aoc2021/test/test_day16.py
@@ -1,10 +1,13 @@
 import aoc2021.day16 as day16
 
+
 def test_adder1():
     assert day16.adder(2, 5) == 7
 
+
 def test_expand1():
     assert day16.expand('D2FE28') == '110100101111111000101000'
+
 
 def test_readliteral1():
     s = '110100101111111000101000'
@@ -13,3 +16,99 @@ def test_readliteral1():
     assert packet['version'] == 6
     assert packet['type'] == 'literal'
     assert packet['val'] == 2021
+
+
+def test_readoperator1():
+    s = '00111000000000000110111101000101001010010001001000000000'
+    reader = day16.Reader(s)
+    packet = reader.read_packet()
+    assert packet['version'] == 1
+    assert packet['type_id'] == 6
+    assert packet['type'] != 'literal'
+
+    children = packet['children']
+    assert len(children) == 2
+    assert children[0]['val'] == 10
+    assert children[1]['val'] == 20
+
+
+def test_readoperator2():
+    s = '11101110000000001101010000001100100000100011000001100000'
+    reader = day16.Reader(s)
+    packet = reader.read_packet()
+    assert packet['version'] == 7
+    assert packet['type_id'] == 3
+    assert packet['type'] != 'literal'
+
+    children = packet['children']
+    assert len(children) == 3
+    assert children[0]['val'] == 1
+    assert children[1]['val'] == 2
+    assert children[2]['val'] == 3
+
+
+def test_nested1():
+    h = '8A004A801A8002F478'
+    s = day16.expand(h)
+    reader = day16.Reader(s)
+    packet = reader.read_packet()
+    assert packet['type'] != 'literal'
+    assert packet['version'] == 4
+
+    children = packet['children']
+    assert children[0]['type'] != 'literal'
+    assert children[0]['version'] == 1
+
+    assert children[0]['children'][0]['type'] != 'literal'
+    assert children[0]['children'][0]['version'] == 5
+
+    assert children[0]['children'][0]['children'][0]['type'] == 'literal'
+    assert children[0]['children'][0]['children'][0]['version'] == 6
+
+    assert day16.sum_versions(packet) == 16
+
+
+def test_nested2():
+    h = '620080001611562C8802118E34'
+    s = day16.expand(h)
+    reader = day16.Reader(s)
+    packet = reader.read_packet()
+    assert packet['type'] != 'literal'
+    assert packet['version'] == 3
+
+    children = packet['children']
+    assert len(children) == 2
+
+    assert children[0]['type'] != 'literal'
+    assert children[1]['type'] != 'literal'
+
+    assert children[0]['children'][0]['type'] == 'literal'
+    assert children[0]['children'][1]['type'] == 'literal'
+
+    assert children[1]['children'][0]['type'] == 'literal'
+    assert children[1]['children'][1]['type'] == 'literal'
+
+    assert day16.sum_versions(packet) == 12
+
+
+def test_nested3():
+    h = 'C0015000016115A2E0802F182340'
+    s = day16.expand(h)
+    reader = day16.Reader(s)
+    packet = reader.read_packet()
+    assert day16.sum_versions(packet) == 23
+
+
+def test_nested4():
+    h = 'A0016C880162017C3686B18A3D4780'
+    s = day16.expand(h)
+    reader = day16.Reader(s)
+    packet = reader.read_packet()
+    assert day16.sum_versions(packet) == 31
+
+def test_calculator1():
+    h = 'C200B40A82'
+    s = day16.expand(h)
+    reader = day16.Reader(s)
+    packet = reader.read_packet()
+    assert day16.eval_packet(packet) == 3   

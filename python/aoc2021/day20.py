11bad04804e3f87913e1552c035f582d04b90ce3
Dan Whitford <dan.whitford@fanduel.com>
2021-12-20 11:16:31 +0000

    --wip-- [skip ci]

--- python/aoc2021/day20.py
+++ python/aoc2021/day20.py
@@ -3,49 +3,45 @@ import os
 
 def read_input(s):
     algo = s.splitlines()[0].strip()
-    
+
     lights = set()
     grid = s.splitlines()[2:]
     for y, row in enumerate(grid):
         for x, light in enumerate(row):
             if light == '#':
-                lights.add((x,y))
+                lights.add((x, y))
 
     return algo, lights
 
 
 def enhance_image(image, algo, step=None):
     new_image = set()
-    min_x = min([p[0] for p in image])
-    max_x = max([p[0] for p in image])
-    min_y = min([p[1] for p in image])
-    max_y = max([p[1] for p in image])
+    min_x = min([x for (x, y) in image])
+    max_x = max([x for (x, y) in image])
+    min_y = min([y for (x, y) in image])
+    max_y = max([y for (x, y) in image])
     for x in range(min_x-1, max_x+2):
         for y in range(min_y-1, max_y+2):
-            if (algo[0] == '#' and algo[-1] == '.') and (x < min_x or x > max_x or y < min_y or y > max_y):
-                if step % 2 == 0:
-                    new_image.add((x, y))
-            else:
-                neighbours = [
-                    (x-1,y-1),
-                    (x, y-1),
-                    (x+1,y-1),
-                    (x-1,y),
-                    (x,y),
-                    (x+1,y),
-                    (x-1,y+1),
-                    (x,y+1),
-                    (x+1,y+1)
-                ]
-                neighbour_string = ''
-                for xx, yy in neighbours:
-                    if (not step) or (min_x <= xx <= max_x and min_y <= yy <= max_y):
-                        neighbour_string += '1' if (xx, yy) in image else '0'
-                    else:
-                        neighbour_string += '0' if step % 2 == 0 else '1'
-                neighbour_num = int(neighbour_string, 2)
-                if algo[neighbour_num] == '#':
-                    new_image.add((x,y))
+            neighbours = [
+                (x-1, y-1),
+                (x, y-1),
+                (x+1, y-1),
+                (x-1, y),
+                (x, y),
+                (x+1, y),
+                (x-1, y+1),
+                (x, y+1),
+                (x+1, y+1)
+            ]
+            neighbour_string = ''
+            for xx, yy in neighbours:
+                if (not step) or (min_x <= xx < max_x and min_y <= yy < max_y):
+                    neighbour_string += '1' if (xx, yy) in image else '0'
+                else:
+                    neighbour_string += '0' if step % 2 == 0 else '1'
+            neighbour_num = int(neighbour_string, 2)
+            if algo[neighbour_num] == '#':
+                new_image.add((x, y))
     return new_image
 
 
